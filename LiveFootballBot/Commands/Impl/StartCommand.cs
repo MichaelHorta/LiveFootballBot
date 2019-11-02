3809c855868a58d013a35d94f64f83b1e928c4f7
Michael Horta Fleitas <michaelhorta32@gmail.com>
2019-11-02 12:20:51 -0400

    Start command

---
+++ LiveFootballBot/Commands/Impl/StartCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiveFootballBot.Commands
+{
+    public class StartCommand : ICommand
+    {
+        public string Execute(CommandParameters parameters)
+        {
+            return "Hello Start";
+        }
+
+        public bool Validate(CommandParameters parameters)
+        {
+            return true;
+        }
+
+        public string GetName()
+        {
+            return "/start";
+        }
+    }
+}
