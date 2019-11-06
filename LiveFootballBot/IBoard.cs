22b526c3494d9140ced55e8620daa136461cdeb5
Michael Horta Fleitas <michaelhorta32@gmail.com>
2019-11-06 18:55:44 -0500

    Abstract methods MatchesBoard, GetMatchBoard and CreateMatchBoard

--- LiveFootballBot/IBoard.cs
+++ LiveFootballBot/IBoard.cs
@@ -1,12 +1,16 @@
-﻿using LiveFootballBot.Models.Events;
-using System;
+﻿using LiveFootballBot.Models;
 using System.Collections.Generic;
-using System.Text;
 
 namespace LiveFootballBot
 {
     public interface IBoard
     {
         List<EventData> SearchEventsData(string date);
+
+        List<MatchBoard> MatchesBoard();
+
+        MatchBoard GetMatchBoard(string matchName);
+
+        MatchBoard CreateMatchBoard(string matchName, string date, long chatId);
     }
 }
