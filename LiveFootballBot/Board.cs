31c8d578eb350064e39ebc5fc62b55ece3365ae4
Michael Horta Fleitas <michaelhorta32@gmail.com>
2019-11-06 18:58:04 -0500

    Injecting services ITelegramBotService. List of match boards

--- LiveFootballBot/Board.cs
+++ LiveFootballBot/Board.cs
@@ -1,4 +1,4 @@
-﻿using LiveFootballBot.Models.Events;
+﻿using LiveFootballBot.Models;
 using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
@@ -9,24 +9,37 @@ namespace LiveFootballBot
     {
         private readonly AppSettings _appSettings;
         private readonly IEventsService _eventsService;
-        public IDictionary<string, List<EventData>> EventsDictionary { get; set; }
+        private readonly ITelegramBotService _telegramBotService;
 
-        public Board(IOptions<AppSettings> config)
+        /// <summary>
+        /// Events information
+        /// </summary>
+        IDictionary<string, List<EventData>> EventsDictionary;
+
+        /// <summary>
+        /// List of board of matches
+        /// </summary>
+        private readonly List<MatchBoard> _matchesBoard;
+
+        public Board(IOptions<AppSettings> config, ITelegramBotService telegramBotService)
         {
             _appSettings = config.Value;
+            _telegramBotService = telegramBotService;
 
             EventsDictionary = new Dictionary<string, List<EventData>>();
             _eventsService = new EventsService();
             var now = DateTime.Now;
             var date = now.ToString("yyyy-MM-dd");
             GetEventsData(date);
+
+            _matchesBoard = new List<MatchBoard>();
         }
 
         public List<EventData> SearchEventsData(string date)
         {
             List<EventData> eventsData;
             EventsDictionary.TryGetValue(date, out eventsData);
-            if(null == eventsData)
+            if (null == eventsData)
             {
                 eventsData = GetEventsData(date);
             }
@@ -35,9 +48,40 @@ namespace LiveFootballBot
 
         private List<EventData> GetEventsData(string date)
         {
-            var eventsData = _eventsService.GetEvents(string.Format($"{_appSettings.Unidadeditorial.Api}&date={date}"), "Football").GetAwaiter().GetResult();
+            var eventsData = _eventsService.GetEvents($"{_appSettings.UnidadeditorialAPI.Events}&date={date}", "Football").GetAwaiter().GetResult();
             EventsDictionary.Add(date, eventsData);
             return eventsData;
+        }
+
+        public List<MatchBoard> MatchesBoard()
+        {
+            return _matchesBoard;
+        }
+
+        public MatchBoard GetMatchBoard(string matchName)
+        {
+            return _matchesBoard.Find(o => o.MatchName == matchName);
+        }
+
+        public MatchBoard CreateMatchBoard(string matchName, string date, long chatId)
+        {
+            var events = SearchEventsData(date);
+            var match = events.Find(ev => matchName.Equals($"{ev.SportEvent.Competitors.HomeTeam.AbbName}-{ev.SportEvent.Competitors.AwayTeam.AbbName}"));
+
+            if (null == match)
+                return null;
+
+            var chatsSubscribed = new List<ChatSubscribed>()
+                {
+                    new ChatSubscribed
+                    {
+                        ChatId = chatId
+                    }
+                };
+            var matchBoard = new MatchBoard(_appSettings, _telegramBotService, _eventsService, match.Id, chatsSubscribed, match.EditorialInfo, match.SportEvent.Competitors);
+            _matchesBoard.Add(matchBoard);
+
+            return matchBoard;
         }
     }
 }
