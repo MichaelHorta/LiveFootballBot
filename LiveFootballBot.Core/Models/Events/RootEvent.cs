4e3980138f45a112085493abf198bc68e14c0e2c
Michael Horta Fleitas <michaelhorta32@gmail.com>
2021-08-13 19:33:23 -0400

    Clases Lineup, Lineups, LineupTeam, SubstitutesActualLineup, ActualLineup, SubstitutesInicialLineup, InicialLineup, PlayerPosition, CContent

--- LiveFootballBot.Core/Models/Events/RootEvent.cs
+++ LiveFootballBot.Core/Models/Events/RootEvent.cs
@@ -16,6 +16,7 @@ namespace LiveFootballBot.Models
         public string Id { get; set; }
         public Event Event { get; set; }
         public DateTime LastUpdate { get; set; }
+        public Lineup Lineup { get; set; }
         public Narration Narration { get; set; }
     }
 
@@ -55,6 +56,80 @@ namespace LiveFootballBot.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+    }
+
+    public class Lineup
+    {
+        public string Id { get; set; }
+        public DateTime LastUpdate { get; set; }
+        public string EventId { get; set; }
+        public Lineups Lineups { get; set; }
+        public bool LineupConfirmed { get; set; }
+    }
+
+    public class Lineups
+    {
+        public LineupTeam AwayTeam { get; set; }
+        public LineupTeam HomeTeam { get; set; }
+    }
+
+    public class LineupTeam
+    {
+        public string Manager { get; set; }
+        public List<SubstitutesActualLineup> SubstitutesActualLineup { get; set; }
+        public List<ActualLineup> ActualLineup { get; set; }
+        public List<SubstitutesInicialLineup> SubstitutesInicialLineup { get; set; }
+        public List<InicialLineup> InicialLineup { get; set; }
+        public string FormationTeam { get; set; }
+        //public TeamInfo TeamInfo { get; set; }
+    }
+
+    public class SubstitutesActualLineup
+    {
+        public bool Titular { get; set; }
+        public string Id { get; set; }
+        public string JerseyNumber { get; set; }
+        public string Name { get; set; }
+        public PlayerPosition PlayerPosition { get; set; }
+    }
+
+    public class ActualLineup
+    {
+        public bool Titular { get; set; }
+        public string Id { get; set; }
+        public string FormationPlace { get; set; }
+        public string JerseyNumber { get; set; }
+        public string Name { get; set; }
+        public PlayerPosition PlayerPosition { get; set; }
+    }
+
+    public class SubstitutesInicialLineup
+    {
+        public string Id { get; set; }
+        public string JerseyNumber { get; set; }
+        public string Name { get; set; }
+        public PlayerPosition PlayerPosition { get; set; }
+    }
+
+    public class InicialLineup : IComparable<InicialLineup>
+    {
+        public string Id { get; set; }
+        public string FormationPlace { get; set; }
+        public string JerseyNumber { get; set; }
+        public string Name { get; set; }
+        public PlayerPosition PlayerPosition { get; set; }
+
+        public int CompareTo(InicialLineup other)
+        {
+            return PlayerPosition.Id - other.PlayerPosition.Id;
+        }
+    }
+
+    public class PlayerPosition
+    {
+        public IDictionary<string, string> AlternateNames { get; set; }
+        public string Name { get; set; }
+        public int Id { get; set; }
     }
 
     public class Narration
@@ -106,7 +181,15 @@ namespace LiveFootballBot.Models
         [JsonProperty(PropertyName = "commentary")]
         public string Text { get; set; }
         public List<object> Multimedia { get; set; }
-        public int Id { get; set; }
+        public long Id { get; set; }
+        [JsonProperty(PropertyName = "content")]
+        public List<CContent> CContent { get; set; }
+    }
+
+    public class CContent
+    {
+        public string Type { get; set; }
+        public string Content { get; set; }
     }
 
     public class NarrationHead
