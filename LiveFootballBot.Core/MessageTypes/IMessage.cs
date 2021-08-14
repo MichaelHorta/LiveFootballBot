665a62e9cb4177dfbd1c44b8b30941a712cc438e
Michael Horta Fleitas <michaelhorta32@gmail.com>
2021-08-14 01:33:07 -0400

    Classes for managment differents ways of messages

---
+++ LiveFootballBot.Core/MessageTypes/IMessage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiveFootballBot.Core.MessageTypes
+{
+    public interface IMessage
+    {
+    }
+}
---
+++ LiveFootballBot.Core/MessageTypes/TextMessage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiveFootballBot.Core.MessageTypes
+{
+    public class TextMessage : IMessage
+    {
+        public string Text { get; set; }
+    }
+}
